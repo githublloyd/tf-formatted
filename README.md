1f62f0a719baa6899d75fdea38329682c49f1900
Lloyd O'Brien <lloydobrien@live.ie>
2019-05-15 16:09:48 +0000

    Add README.md to the Repo

---
+++ README.md
@@ -0,0 +1,10 @@
+### Terraform
+
+## Terraform Multi Environment Setup using Modules
+
+# The benefits of using a Module-based Terraform code struture
+
+> Abstract and reusable modules
+> Envrionments are composed of a selection of modules
+> Make changes to one environment without affecting another, limiting blast radius
+> Each environment has its own .tfstate file
--- modules/network/network.tf
+++ modules/network/network.tf
@@ -5,18 +5,18 @@ resource "aws_vpc" "vpc-base" {
 }
 
 resource "aws_subnet" "red" {
-  vpc_id    = "${aws_vpc.vpc-base.id}"
-  cidr_block    = "${var.red_cidr}"
+  vpc_id                = "${aws_vpc.vpc-base.id}"
+  cidr_block            = "${var.red_cidr}"
   availability_zone     = "${var.red_az}"
 }
 
 resource "aws_subnet" "amber" {
-  vpc_id    = "${aws_vpc.vpc-base.id}"
-  cidr_block    = "${var.amber_cidr}"
+  vpc_id                = "${aws_vpc.vpc-base.id}"
+  cidr_block            = "${var.amber_cidr}"
   availability_zone     = "${var.amber_az}"
 }
 resource "aws_subnet" "green" {
-  vpc_id    = "${aws_vpc.vpc-base.id}"
-  cidr_block    = "${var.green_cidr}"
+  vpc_id                = "${aws_vpc.vpc-base.id}"
+  cidr_block            = "${var.green_cidr}"
   availability_zone     = "${var.green_az}"
 }
