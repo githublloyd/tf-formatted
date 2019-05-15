df10077f1641dce4c327ed7e7f0d9a100368456a
Lloyd O'Brien <lloydobrien@live.ie>
2019-05-15 16:17:52 +0000

    Rename variables.tf to input.tf and populate to reference in VPC Network Module

---
+++ modules/network/input.tf
@@ -0,0 +1,9 @@
+variable "cidr" {}
+variable "dns" {}
+variable "dnsh" {}
+variable "red_cidr" {}
+variable "red_az" {}
+variable "amber_cidr" {}
+variable "amber_az" {}
+variable "green_cidr" {}
+variable "green_az" {}
