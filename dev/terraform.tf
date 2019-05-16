959c72e6ed91af3db416a4aff876ce2c60988ee7
Lloyd O'Brien <lloydobrien@live.ie>
2019-05-16 09:34:32 +0000

    Adding compute/ Module and renaming network/ .tf config file

--- dev/terraform.tf
+++ dev/terraform.tf
@@ -1,4 +1,8 @@
 module "network" {
   source = "../../modules/network/main.tf"
   
+  cidr = "${var.cidr}"
+  red_cidr = "${var.red_cidr}"
+  amber_cidr = "${var.amber_cidr}"
+  green_cidr = "${var.green_cidr}"
 }
