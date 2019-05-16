561e332a449cba2bce3aa798837cd497643f2f34
Lloyd O'Brien <lloydobrien@live.ie>
2019-05-16 13:13:59 +0000

    Clearing prd and stg content

--- dev/config.tf
+++ dev/config.tf
@@ -1,9 +1,7 @@
 "cidr": "10.0.0.0/16",
 "dns": "true",
 "dnsh": "true",
+"az-irl": "eu-west-1",
 "red_cidr": "10.0.0.0/24",
-"red_az": "",
 "amber_cidr": "10.0.1.0/24",
-"amber_az": "",
-"green_cidr": "10.0.2.0/24",
-"green_az": "",
+"green_cidr": "10.0.2.0/24"
--- modules/network/main-ntwrk.tf
+++ modules/network/main-ntwrk.tf
@@ -32,7 +32,7 @@ resource "aws_route_table" "red_rt" {
 }
 resource "aws_route" "igw-route" {
   route_table_id        = "${aws_route_table.red_rt.id}"
-  destination_cidr_block= "${0.0.0.0/0}"
+  destination_cidr_block= "0.0.0.0/0"
   gateway_id            = "${aws_internet_gateway.igw.id}"
 }
 ### Route Table for Amber Subnet ###
