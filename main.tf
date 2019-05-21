90816ec5bc4ed1cb229621e9f87ba5ba9c6a0c61
Lloyd O'Brien <lloydobrien@live.ie>
2019-05-21 11:28:24 +0000

    Add S3 backend for remote state

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ module "dev" {
   source = "dev/"
   
 }
+
+terraform {
+  backend "s3" {
+    bucket      = "lloydob-v1bucket"
+    key         = "terraform.tfstate"
+    region         = "eu-west-1"
+    encrypt = true
+  }
+}
