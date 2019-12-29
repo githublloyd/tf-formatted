ca5410805e53bb7112bc74b41af760b668062481
Lloyd O'Brien <33862350+githublloyd@users.noreply.github.com>
2019-12-29 21:07:29 +0000

    Remove bucket name

--- dev/init.tf
+++ dev/init.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket      = "lloydob-v1bucket"
+    bucket      = "BUCKETNAME"
     key         = "dev/terraform.tfstate"
     region         = "eu-west-1"
     encrypt = true
